c60eaa31f123ebf53cc168ce1036ad0042e56027
mocominette <m0c0@tuta.io>
2026-07-05 14:14:39 +0900

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,14 @@
 ///
 /// This is the best implementation of this program to ever exist.
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_helo() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
